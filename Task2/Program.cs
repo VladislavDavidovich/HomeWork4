f741b86df7f976c49b5c384d39742f2d01098607
Vladislav <Vlad.davidovich.2014@mail.ru>
2022-08-22 21:50:49 +0300

    Сохранение второй задачи

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Ведите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+string firstgNumber = Convert.ToString(number);
+int sum = 0;
+
+for(int i = 0; i < firstgNumber.Length; i++)
+{
+    string temp = Convert.ToString(firstgNumber[i]);
+    sum += Convert.ToInt32(temp);
+}
+Console.WriteLine(sum);
