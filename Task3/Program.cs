d14218a98b2d68e7c931d780af575116b968267b
Vladislav <Vlad.davidovich.2014@mail.ru>
2022-08-22 22:46:01 +0300

    Сохранение третьей задачи

---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int [number];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] =  new Random().Next(-100,100);
+    Console.Write(array[i] + " ");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int [] = new Random();
+
+//for(int i = 0; i<arr.Length; i++)
+
+//arr[i] = rand.Next(-10,10);
